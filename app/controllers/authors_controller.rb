8909f8f9a3dd82d8e6e9e714791a9a9bb99395e8
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-09-11 00:49:59 +0000

    Automatically backed up by Learn

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -12,7 +12,7 @@ class AuthorsController < ApplicationController
 
   def post
     @author = Author.find(params[:id])
-    @post = Post.find(params[:id])
+    @post = Post.find(params[:post_id])
     render template: 'posts/show'
   end
 
